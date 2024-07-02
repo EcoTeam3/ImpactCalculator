cfd553c99c917704a7ddd18b465d343fa0dd369f
Hamidjon Nuriddinov <nuriddinovhamidjon2@gmail.com>
2024-07-02 14:53:01 +0500

    Test qilish uchun tayyor

--- server/main.go
+++ server/main.go
@@ -1,6 +1,35 @@
 package main
 
+import (
+	"impactcalculator/config"
+	pb "impactcalculator/generated"
+	"impactcalculator/service"
+	"impactcalculator/storage"
+	"impactcalculator/storage/postgres"
+	"log"
+	"net"
 
-func main(){
-	
+	"google.golang.org/grpc"
+)
+
+func main() {
+	db, err := storage.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	config := config.Load()
+	listener, err := net.Listen("tcp", ":"+config.URL_PORT)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listener.Close()
+
+	s := service.NewImpactRepo(*postgres.NewImpactRepo(db))
+	grpc := grpc.NewServer()
+	pb.RegisterImpactServer(grpc, s)
+	if err = grpc.Serve(listener); err != nil {
+		log.Fatal(err)
+	}
 }
